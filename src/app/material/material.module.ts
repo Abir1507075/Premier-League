26cb1bf4e22497e22fabe2607383ca43da4cd04a
Abid Hasan Abir <abidhasanabir19@gmail.com>
2021-03-07 17:11:36 +0600

    added matchdata service

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 
 const MaterialComponents = [
   MatButtonModule,
   MatListModule,
+  MatPaginatorModule,
 ];
 
 
